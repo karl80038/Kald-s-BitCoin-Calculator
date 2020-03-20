526312837935ed495351b80ec84502855c3775b5
Karl-Erik Kald <karl80038@gmail.com>
2020-03-20 14:19:06 +0200

    For troubleshooting purposes, revert the Port number to a static value

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ app.use(function(req, res, next){
 // app.listen(port, ()=> {
 //     console.log(`Server is running on port ${port}."`);
 // })
-app.listen(process.env.PORT || port, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server has started");
 })
